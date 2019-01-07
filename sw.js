ed77a22eed0df864bde331c886c79695d440716e
Ricardo Marin <ricardomarinpalacios@gmail.com>
2019-01-07 13:05:22 -0300

    Modificamos el sw

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts ('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE= 'dynamic-v1';
+const DYNAMIC_CACHE= 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -38,18 +38,26 @@ self.addEventListener('install' , e => {
     e.waitUntil( Promise.all([cacheStatic, cacheInmutable]));
 });
 
-self.addEventListener('activate' , e => {
-    
-const respuesta = caches.keys().then(keys => {
-    keys.forEach(key => {
-        if(key !== STATIC_CACHE && key.includes('static')){
-            return caches.delete(key);
-        }
+self.addEventListener('activate', e => {
+ 
+    const respuesta = caches.keys().then( keys => {
+ 
+        keys.forEach( key => {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
+        });
+ 
     });
-});
-
-    e.waitUntil(respuesta);
-
+ 
+    e.waitUntil( respuesta );
+ 
 });
 
 self.addEventListener('fetch' , e => {
